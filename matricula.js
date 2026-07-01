4e6c041f91d442b278008fadd33db40fa16ae97b
richardkaian <richard.kaian777@gmail.com>
2026-06-30 22:56:10 -0300

    Update matricula.js

--- matricula.js
+++ matricula.js
@@ -4,19 +4,19 @@ let disciplinas = [
     "JavaScript"
 ];
 
-// Exibir disciplinas
+
 disciplinas.forEach(disciplina => console.log(disciplina));
 
 console.log(`\nTotal: ${disciplinas.length}`);
 
-// Verificar JavaScript
+
 if (disciplinas.includes("JavaScript")) {
     console.log("\nAluno cursa JavaScript");
 } else {
     console.log("\nAluno não cursa JavaScript");
 }
 
-// Adicionar disciplina
+
 disciplinas.push("Engenharia de Software");
 
 console.log("\nPercorrendo disciplinas:");
