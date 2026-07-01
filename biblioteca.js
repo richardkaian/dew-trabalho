2c3ede511943e323315e38af9a7956db8f55c30e
richardkaian <richard.kaian777@gmail.com>
2026-06-30 22:55:09 -0300

    Display authors list using join method
    Added join method to display authors list.

--- biblioteca.js
+++ biblioteca.js
@@ -1,13 +1,13 @@
 const autores =
 "Machado de Assis,Clarice Lispector,Monteiro Lobato";
 
-// Transformar em array
+
 const listaAutores = autores.split(",");
 
-// Exibir autores
+
 listaAutores.forEach(autor => console.log(autor));
 
-// Verificar autor
+
 const pesquisa = "Clarice Lispector";
 
 if (listaAutores.includes(pesquisa)) {
@@ -16,13 +16,13 @@ if (listaAutores.includes(pesquisa)) {
     console.log("\nAutor não encontrado");
 }
 
-// Função
+
 function cadastrarLivro(titulo, autor) {
     return `Livro "${titulo}" escrito por ${autor}.`;
 }
 
 console.log(cadastrarLivro("Dom Casmurro", "Machado de Assis"));
 
-// Join
+
 console.log("\nLista:");
 console.log(listaAutores.join(", "));
